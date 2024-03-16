db07fe7278cb5c377afa1e0aceca3825b6b0bfd9
baharkose <baharkse17@gmail.com>
2024-03-17 02:20:06 +0300

    feat: skip

--- 18-BlogAPI_SearchSortPage/src/controllers/blog.controller.js
+++ 18-BlogAPI_SearchSortPage/src/controllers/blog.controller.js
@@ -148,20 +148,26 @@ module.exports.BlogPost = {
 
     // // Page:
     let page = Number(req.query?.page);
-    page = page > 0 ? page : 1;
+    // page = page > 0 ? page : 1;
+    page = page > 0 ? page - 1 : 0;
+    // backendde sayfa sayısı her zaman page - 1 olarak hesaplanmalı.
+    // kullanıcı onu 1 görücek ama onu ben 0 olarak görücem peki atlama nasıl olucak?
+    // page*limit
     console.log(page);
 
     // SKİP
     // LIMIT 10, 10 -> İlk 10 kaydı atla ve 2. 10 kaydı getir. 10. kayıttan sonra 10 kaydı getir.
+    // her zaman page kaç ise 1 eksiği olması gerekir.
 
     let skip = Number(req.query?.skip);
-    skip = skip > 0 ? skip :
+
+    skip = skip > 0 ? skip : page * limit;
 
     // page 1 ise atlanacak kayıt sayısı 0, page 3 dediğimizde atlanacak kayıt sayısı 40
 
     const data = await BlogPost.find({ ...filter, ...search })
       .sort(sort)
-      .skip(10)
+      .skip(skip)
       .limit(limit);
     // ilk 10 kaydı atla limit olanı getir.
 
