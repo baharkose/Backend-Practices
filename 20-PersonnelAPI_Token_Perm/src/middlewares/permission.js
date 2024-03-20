e87dc5618e032e732ba8f2d0a5402ec4fd1cb22e
baharkose <baharkse17@gmail.com>
2024-03-21 00:12:58 +0300

    feat: identifying permissions

--- 20-PersonnelAPI_Token_Perm/src/middlewares/permission.js
+++ 20-PersonnelAPI_Token_Perm/src/middlewares/permission.js
@@ -17,6 +17,29 @@ module.exports = {
       throw new Error("No Permission: You must login");
     }
   },
-  isAdmin: (req, res, next) => {},
-  isLead: (req, res, next) => {},
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+  isLead: (req, res, next) => {
+    // sadece kendi departmanlarını görebilir. önce departman idsini almamız lazım. reqden id olarak gelicek veya null gelicek. bana idnin gelmesi lazım. urlden idnin urlden geldiğini biliyorum onu almam lazım. mutlaka bir id gelmeli urlden
+    // giriş yapmak zorunda -aktif olmalı ve admin olmalu yada admin değilse lead true olmalı ve userın departmanid'si req.userdan gelen departmentId'ye eşit olmak zorunda
+    if (
+      req.user &&
+      req.user.isActive &&
+      (req.user.isAdmin ||
+        (req.user.isLead && req.user.deparmentId == departmentId))
+    ) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error(
+        "NoPermission: You must login and to be Admin or Department Lead."
+      );
+    }
+  },
 };
