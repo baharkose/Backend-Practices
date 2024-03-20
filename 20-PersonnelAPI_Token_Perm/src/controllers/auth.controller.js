eb0306f6ef7026855b0a5e2a4ba8f4a10cdbffbd
baharkose <baharkse17@gmail.com>
2024-03-20 22:49:13 +0300

    feat: tokenkey -> authcontroller

--- 20-PersonnelAPI_Token_Perm/src/controllers/auth.controller.js
+++ 20-PersonnelAPI_Token_Perm/src/controllers/auth.controller.js
@@ -16,14 +16,33 @@ module.exports = {
     if (username && password) {
       const user = await Personnel.findOne({ username, password });
       if (user) {
+        // * SESSION
         // Set Session:
-        req.session = {
-          id: user._id,
-          password: user.password,
-        };
-        // Set Cookie:
-        if (req.body?.rememberMe) {
-          req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3; // 3 Days
+        // req.session = {
+        //   id: user._id,
+        //   password: user.password,
+        // };
+        // // Set Cookie:
+        // if (req.body?.rememberMe) {
+        //   req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3; // 3 Days
+        // }
+
+        // kullanıcın verilerini bir sesiona değil bir tokena havale etmemiz lazım bunu nasıl yapacağız?
+
+        // bir modele ihtiyaç var. personel modele
+
+        // * SESSION
+
+        // TOKEN
+        // token daha önce oluşturulmuş mu bunu kontrol etmemiz lazım. Mcelikle bunun daha önceden bir tokenı var mı kontrol edeceğiz
+
+        let tokenData = await Token.findOne({ userId: user._id });
+
+        // eğer token yoksa oluştur - key yap onu al. burada benzersiz bir şey koymamız lazım.
+
+        if (!tokenData) {
+          const tokenKey = user._id + Date.now(); // benzersiz bir token adresi tanımladık.
+          tokenData = await Token.create({ userId: user._id, token: tokenKey });
         }
 
         res.status(200).send({
@@ -49,5 +68,3 @@ module.exports = {
     });
   },
 };
-
-
--- 20-PersonnelAPI_Token_Perm/src/models/token.model.js
+++ 20-PersonnelAPI_Token_Perm/src/models/token.model.js
@@ -1,50 +1,88 @@
+// "use strict";
+// /* -------------------------------------------------------
+//     EXPRESS - Personnel API
+// ------------------------------------------------------- */
+
+// // ! 1 önce kullanıcı giriş yapıcak doğru ise onun için token oluşturucaz.
+
+// const { mongoose } = require("../configs/dbConnection");
+// // modülü tekrar tekrar çağırmamak için bu şekilde mongooseu çağııroruz.
+
+// /*------------------------------------------------------- *
+// // user Id ben userı saklayacağım, tokenda ise oluşturduğum token olacak.
+// {
+//   "userId": "65343222b67e9681f937f001",
+//   "token": "...tokenKey..."
+// }
+// /* ------------------------------------------------------- */
+
+// // Token Model:
+
+// const TokenSchema = new mongoose.Schema(
+//   {
+//     userId: {
+//       type: mongoose.Schema.ObjectId,
+//       //   bu user modelinin objectIdsi
+//       ref: "Personnel",
+//       required: "true",
+//       index: "true",
+//       //   neden index true -> daha hızlı erişebilecek şekilde saklar. Ramden destek alır. Ram hdd'den daha hızlı. O da veriyi rame saklıyor ve daha hızlı bir erişim sağlıyor
+//     },
+//     token: {
+//       type: String,
+//       tirm: true,
+//       required: true,
+//       // özellikle index true çok önemli, özellikle SQL dblerde bu çok önemli
+//       index: true,
+//       unique: true,
+//     },
+//   },
+//   {
+//     collection: "tokens",
+//     timestamps: true,
+//   }
+// );
+
+// // model ismi inctance , ilk harfleri de büyük PascalCase, controller camelCase
+// module.exports = mongoose.model("Token", TokenSchema);
+// // şemamızı model haline döküyoruz. Şemayı token isminde bir modele çevir ve token şemayı çek.
+
+// // ! her kullanıcının varlığını token ile anlayacağız... normalde tokena controller yazmaya gerek yok. heyecan .. 
 "use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
-
-// ! 1 önce kullanıcı giriş yapıcak doğru ise onun için token oluşturucaz.
-
-const { mongoose } = require("../configs/dbConnection");
-// modülü tekrar tekrar çağırmamak için bu şekilde mongooseu çağııroruz.
-
-/*------------------------------------------------------- *
-// user Id ben userı saklayacağım, tokenda ise oluşturduğum token olacak.
+const { mongoose } = require('../configs/dbConnection')
+/* ------------------------------------------------------- *
 {
-  "userId": "65343222b67e9681f937f001",
-  "token": "...tokenKey..."
+    "userId": "65343222b67e9681f937f001",
+    "token": "...tokenKey..."
 }
 /* ------------------------------------------------------- */
-
 // Token Model:
 
-const TokenSchema = new mongoose.Schema(
-  {
+const TokenSchema = new mongoose.Schema({
+
     userId: {
-      type: mongoose.Schema.ObjectId,
-      //   bu user modelinin objectIdsi
-      ref: "Personnel",
-      required: "true",
-      index: "true",
-      //   neden index true -> daha hızlı erişebilecek şekilde saklar. Ramden destek alır. Ram hdd'den daha hızlı. O da veriyi rame saklıyor ve daha hızlı bir erişim sağlıyor
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Personnel', // 'User'
+        required: true,
+        index: true,
+        unique: true,
     },
+
     token: {
-      type: String,
-      tirm: true,
-      required: true,
-      // özellikle index true çok önemli, özellikle SQL dblerde bu çok önemli
-      index: true,
-      unique: true,
+        type: String,
+        trim: true,
+        required: true,
+        index: true,
+        unique: true,
     },
-  },
-  {
-    collection: "tokens",
-    timestamps: true,
-  }
-);
 
-// model ismi inctance , ilk harfleri de büyük PascalCase, controller camelCase
-module.exports = mongoose.model("Token", TokenSchema);
-// şemamızı model haline döküyoruz. Şemayı token isminde bir modele çevir ve token şemayı çek.
+}, {
+    collection: 'tokens',
+    timestamps: true
+})
 
-// ! her kullanıcının varlığını token ile anlayacağız... normalde tokena controller yazmaya gerek yok. heyecan .. 
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('Token', TokenSchema)
