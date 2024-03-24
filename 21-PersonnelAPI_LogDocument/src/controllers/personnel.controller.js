ea0b8de28af61211cf6ce161374489b8dcf63726
baharkose <baharkse17@gmail.com>
2024-03-24 13:21:53 +0300

    feat: personelController isAdmin condition for updating operations

--- 21-PersonnelAPI_LogDocument/src/controllers/personnel.controller.js
+++ 21-PersonnelAPI_LogDocument/src/controllers/personnel.controller.js
@@ -1,83 +1,97 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESS - Personnel API
 ------------------------------------------------------- */
 
-const Personnel = require('../models/personnel.model')
+const Personnel = require("../models/personnel.model");
 
 module.exports = {
+  list: async (req, res) => {
+    const data = await res.getModelList(Personnel, {}, "departmentId");
 
-    list: async (req, res) => {
+    res.status(200).send({
+      error: false,
+      detail: await res.getModelListDetails(Personnel),
+      data,
+    });
+  },
 
-        const data = await res.getModelList(Personnel, {}, 'departmentId')
+  create: async (req, res) => {
+    // isLead Control:
+    const isLead = req.body?.isLead || false;
+    if (isLead) {
+      const xyz = await Personnel.updateMany(
+        { departmentId: req.body.departmentId, isLead: true },
+        { isLead: false }
+      );
+    }
 
-        res.status(200).send({
-            error: false,
-            detail: await res.getModelListDetails(Personnel),
-            data
-        })
+    const data = await Personnel.create(req.body);
 
-    },
+    res.status(201).send({
+      error: false,
+      data,
+    });
+  },
 
-    create: async (req, res) => {
+  read: async (req, res) => {
+    const data = await Personnel.findOne({ _id: req.params.id });
 
-        // isLead Control:
-        const isLead = req.body?.isLead || false
-        if (isLead) {
-            const xyz = await Personnel.updateMany({ departmentId: req.body.departmentId, isLead: true }, { isLead: false })
-        }
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
 
-        const data = await Personnel.create(req.body)
+  update: async (req, res) => {
+    // ! eğer gelen kişi admin değilse kendini lead yapma ve maaşını güncelleyemesin. kendi kaydını güncelleyebilir ama isLead olma ve maaş kısmını yalnızca admin değiştirebilir.
+    if (!req.user.isAdmin) {
+      // istersem sabit bir değer veririm istersem de direk delete ile geçersiz kılarım.
+      req.body.isLead = false;
+      req.body.isAdmin = false;
+      //   gelen değeri iptal et geçersiz kıl
+      delete req.body.isLead;
+      delete req.body.salary;
+    }
 
-        res.status(201).send({
-            error: false,
-            data
-        })
+    update2: async (req, res) => {
+      if (!req.user.isAdmin) {
+        req.body.isAdmin = false;
+        delete req.body.salary;
+        delete req.body.isLead;
+      }
+    };
 
-    },
+    // isLead Control:
+    const isLead = req.body?.isLead || false;
+    if (isLead) {
+      const { departmentId } = await Personnel.findOne(
+        { _id: req.params.id },
+        { departmentId: 1 }
+      );
+      await Personnel.updateMany(
+        { departmentId, isLead: true },
+        { isLead: false }
+      );
+    }
 
-    read: async (req, res) => {
+    const data = await Personnel.updateOne({ _id: req.params.id }, req.body, {
+      runValidators: true,
+    });
 
-        const data = await Personnel.findOne({ _id: req.params.id })
+    res.status(202).send({
+      error: false,
+      data,
+      new: await Personnel.findOne({ _id: req.params.id }),
+    });
+  },
 
-        res.status(200).send({
-            error: false,
-            data
-        })
+  delete: async (req, res) => {
+    const data = await Personnel.deleteOne({ _id: req.params.id });
 
-    },
-
-    update: async (req, res) => {
-
-        if (!req.user.isAdmin) {
-            req.body.isAdmin = false
-            delete req.body.isLead
-            delete req.body.salary
-        }
-
-        // isLead Control:
-        const isLead = req.body?.isLead || false
-        if (isLead) {
-            const { departmentId } = await Personnel.findOne({ _id: req.params.id }, { departmentId: 1 })
-            await Personnel.updateMany({ departmentId, isLead: true }, { isLead: false })
-        }
-
-        const data = await Personnel.updateOne({ _id: req.params.id }, req.body, { runValidators: true })
-
-        res.status(202).send({
-            error: false,
-            data,
-            new: await Personnel.findOne({ _id: req.params.id })
-        })
-    },
-
-    delete: async (req, res) => {
-
-        const data = await Personnel.deleteOne({ _id: req.params.id })
-
-        res.status(data.deletedCount ? 204 : 404).send({
-            error: !data.deletedCount,
-            data
-        })
-    },
-}
+    res.status(data.deletedCount ? 204 : 404).send({
+      error: !data.deletedCount,
+      data,
+    });
+  },
+};
--- 21-PersonnelAPI_LogDocument/src/middlewares/permissions.js
+++ 21-PersonnelAPI_LogDocument/src/middlewares/permissions.js
@@ -1,64 +1,63 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
 // Middleware: permissions (authorization)
 
 module.exports = {
-
-    isLogin: (req, res, next) => {
-
-        if (req.user && req.user.isActive) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login.')
-        }
-    },
-
-    isAdmin: (req, res, next) => {
-
-        if (req.user && req.user.isActive && req.user.isAdmin) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Admin.')
-        }
-    },
-
-    isAdminOrLead: (req, res, next) => {
-
-        const departmentId = req.params?.id
-
-        if (
-            req.user
-            && req.user.isActive
-            && (req.user.isAdmin || (req.user.isLead && req.user.departmentId == departmentId))
-        ) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Admin or Department Lead.')
-        }
-    },
-
-    isAdminOrOwn: (req, res, next) => {
-
-        const personnelId = req.params?.id
-
-        if (
-            req.user
-            && req.user.isActive
-            && (
-                req.user.isAdmin
-                || req.user._id == personnelId
-            )
-        ) {
-            next()
-        } else {
-            res.errorStatusCode = 403
-            throw new Error('NoPermission: You must login and to be Admin or Record Owner.')
-        }
-
+  isLogin: (req, res, next) => {
+    if (req.user && req.user.isActive) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login.");
     }
-}
+  },
+
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be Admin.");
+    }
+  },
+
+  isAdminOrLead: (req, res, next) => {
+    const departmentId = req.params?.id;
+
+    if (
+      req.user &&
+      req.user.isActive &&
+      (req.user.isAdmin ||
+        (req.user.isLead && req.user.departmentId == departmentId))
+    ) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error(
+        "NoPermission: You must login and to be Admin or Department Lead."
+      );
+    }
+  },
+
+  // + kendi kaydını görüntüleme, güncelleme ama bunun kendini admin yapamaması lazım bunu nasıl engelleriz. Bunun için güncelleme işleminin yapıldığı personelControllera gidelim.
+  isAdminOrOwn: (req, res, next) => {
+    // bu personelId bana nerden geliyor? urlden geliyor onu aldım.
+    const personnelId = req.params?.id;
+
+    if (
+      req.user &&
+      req.user.isActive &&
+      //   gelen personelId, modeldeki idye eşit mi?
+      (req.user.isAdmin || req.user._id == personnelId)
+    ) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error(
+        "NoPermission: You must login and to be Admin or Record Owner."
+      );
+    }
+  },
+};
